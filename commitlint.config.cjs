233b7a4dae28650923f860a582b6cd02c3f972da
Polina Zakrevskaya <ppzakrevskaya@gmail.com>
2026-06-11 16:17:31 +0300

    feat: add stage 1

---
+++ commitlint.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'perf', 'ci', 'build'],
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+    'subject-min-length': [2, 'always', 3],
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Modsen Calendar</h1>
+      <p>Календарь событий</p>
+    </div>
+  );
+}
+
+export default App;
